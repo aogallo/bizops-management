37b8a352c5fcc916986685df0ae7aced1b017a11
Allan Gallo <allan.gallo@3pillarglobal.com>
2025-10-23 18:29:40 -0600

    chore: lint staged and commit lint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: "@commitlint/format",
+};
